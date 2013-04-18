d4d57f9f57ee54b5a27faa0916dd969a1e78136c
Amio <amio.cn@gmail.com>
2013-04-18 15:39:01 +0800

    add missing hotel template and view.

---
+++ dev/scripts/views/hotel.js
@@ -0,0 +1,20 @@
+/**
+ * Hotel View
+ */
+define(['$', '_', 'B', 'BV', 'text!templates/hotel.html'], function ($, _, B, BaseView, hotelTmpl){
+  'use strict';
+
+  var IndexView = BaseView.extend({
+    className: 'hotel-view',
+    initialize: function (args){
+      this.render(args);
+    },
+    render: function (renderArgs){
+      this.$el.html(_.template(hotelTmpl)(renderArgs));
+      this.renderPage();
+      return this;
+    }
+  });
+
+  return IndexView;
+});
